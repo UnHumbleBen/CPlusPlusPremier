6ffbebf484cb61bcb4f9ed4c3c3b133d79d09b9b
Benjamin Lee <bnllee@ucdavis.edu>
2018-12-31 11:22:58 -0800

    added comments

--- Chapter1/prog2.cc
+++ Chapter1/prog2.cc
@@ -1,11 +1,17 @@
 #include <iostream>
+/*
+ * Simple main function:
+ * Read two numbers and write their sum
+ */
 int main()
 {
+    // prompt user to enter two numbers
     std::cout << "Enter two numbers:" << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
+    int v1 = 0, v2 = 0;   // variables to hold the input we read
+    std::cin >> v1 >> v2; // read input
     std::cout << "The product of " << v1;
     std::cout               << " and " << v2;
     std::cout           << " is " << v1 * v2 << std::endl;
+
     return 0;
 }
