8e637759a0f0576af8b65e911167b1729442dbf5
Benjamin Lee <bnllee@ucdavis.edu>
2019-01-01 17:33:42 -0800

    1.4.4

--- Chapter1/range.cc
+++ Chapter1/range.cc
@@ -3,6 +3,14 @@ int main() {
     std::cout << "Enter two numbers:" << std::endl;
     int start, end;
     std::cin >> start >> end;
+    
+    // handling if start is greater than end
+    if (start > end) {
+        int temp = start;
+        start = end;
+        end = temp;
+    }
+
     std::cout << "The numbers between " << start 
               << " and " << end << " are" << std::endl;
     for (int num = start; num <= end; ++num) {
