e9b39bb96dfbaa1933de7c45208b4369b68b2f6a
Benjamin Lee <bnllee@ucdavis.edu>
2019-01-04 17:38:22 -0800

    2.2.3 names

---
+++ Chapter2/invalid_names.cc
@@ -0,0 +1,7 @@
+int main() {
+    int double = 3.14; // invalid, double is reserved
+    int _; // valid
+    int catch-22; // invalid, hypen
+    int 1_or_2 = 1; // invalid, begins with a number
+    double Double = 3.14; // valid
+}
